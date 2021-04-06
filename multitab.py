c29dbd35992b2a446e720b0342494a3d9dffe2fb
Theninpan <61723696+Theninpan@users.noreply.github.com>
2021-04-06 11:42:54 +0530

    Add files via upload

--- multitab.py
+++ multitab.py
@@ -1,7 +1,10 @@
 import tkinter as tk                    
 from tkinter import ttk
-  
-  
+import os
+from tkinter import *
+from tkinter.messagebox import *
+from tkinter.filedialog import *
+
 root = tk.Tk()
 root.title("Tab Widget")
 tabControl = ttk.Notebook(root)
@@ -12,18 +15,15 @@ tab2 = ttk.Frame(tabControl)
 tabControl.add(tab1, text ='Tab 1')
 tabControl.add(tab2, text ='Tab 2')
 tabControl.pack(expand = 1, fill ="both")
-  
-ttk.Label(tab1, 
-          text ="Welcome to \
-          GeeksForGeeks").grid(column = 0, 
-                               row = 0,
-                               padx = 30,
-                               pady = 30)  
-ttk.Label(tab2,
-          text ="Lets dive into the\
-          world of computers").grid(column = 0,
-                                    row = 0, 
-                                    padx = 30,
-                                    pady = 30)
-  
+__thisTextArea=[]
+__thisScrollBar=[]
+__thisTextArea.append(Text(tab1, undo=True))
+__thisTextArea.append(Text(tab2, undo=True))
+__thisTextArea[tabControl.index(tabControl.select())].grid(sticky=N + E + S + W)
+__thisScrollBar.append(Scrollbar(__thisTextArea[tabControl.index(tabControl.select())]))
+def onclick(event):
+    __thisTextArea[tabControl.index(tabControl.select())].grid(sticky=N + E + S + W)
+    __thisScrollBar.append(Scrollbar(__thisTextArea[tabControl.index(tabControl.select())]))
+
+tabControl.bind("<<NotebookTabChanged>>",onclick)
 root.mainloop()
