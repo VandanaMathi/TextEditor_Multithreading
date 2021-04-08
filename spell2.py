58d2b8a81877a22c7dda72655979e3ac768a1697
Theninpan <61723696+Theninpan@users.noreply.github.com>
2021-04-08 17:57:36 +0530

    Delete spell2.py

--- spell2.py
+++
@@ -1,8 +0,0 @@
-from spellchecker import SpellChecker
-
-spell = SpellChecker()  # loads default word frequency list
-spell.word_frequency.load_text_file('./my_free_text_doc.txt')
-
-# if I just want to make sure some words are not flagged as misspelled
-spell.word_frequency.load_words(['microsoft', 'apple', 'google'])
-spell.known(['microsoft', 'google'])  # will return both now!
